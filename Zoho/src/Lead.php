070800d98f24ea5f800d92f1f3763364fc296bda
Kevin Ruscoe <kevin@popcreative.co.uk>
2017-03-23 12:11:56 +0000

    init

---
+++ Zoho/src/Lead.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Zoho;
+
+use GuzzleHttp\Client;
+
+class Lead {
+
+    private $token;
+    private $endpoint;
+    private $client;
+
+    /**
+     * Create a new client.
+     *
+     * @param string $token Authentication Token
+     **/
+    public function __construct($token)
+    {
+        $this->endpoint = 'https://crm.zoho.com/crm/private/json/Leads/';
+        $this->token = $token;
+
+        $this->client = new Client([
+            'base_uri' => $this->endpoint,
+        ]);
+    }
+
+    /**
+     * Creates a new Lead
+     *
+     * Argument list can be found at https://www.zoho.com/crm/help/customization/standard-fields.html#Leads
+     *
+     * @param array $args The fields to create a new lead from.
+     *
+     * @return mixed False if failed, the response from Zoho if success
+     **/
+    public function create($args = [])
+    {
+
+        $args = [
+            'xmlData' => $args
+        ];
+
+        $query = Utils::normaliseArray([
+            'authtoken' => $this->token,
+            'xmlData' => [
+                'Lead Source' => 'Testing API',
+                'Company' => 'Testing',
+                'First Name' => 'Testing',
+                'Last Name' => 'API',
+                'Email' => 'testing@api.testing',
+                'Primary Phone' => '00000000000',
+            ]
+        ], $args);
+
+        $query['xmlData'] = Utils::toXmlEntity('Lead', $query['xmlData']);
+
+        $response = $this->client->post('insertRecords', [
+            'query' => $query
+        ]);
+
+        if ($response->getStatusCode() !== 200) {
+            return false;
+        }
+
+        return json_decode($response->getBody())->response->result->recorddetail;
+
+    }
+
+    public function get($args = [])
+    {
+
+        $response = $this->client->get('getRecords', [
+            'query' => Utils::normaliseArray([
+                'authtoken' => $this->token,
+                'fromIndex' => 0,
+                'toIndex' => 10
+            ], $args)
+        ]);
+
+        if ($response->getStatusCode() !== 200) {
+            return false;
+        }
+
+        return json_decode($response->getBody())->response->result->Leads->row;
+
+    }
+
+}
---
+++ Zoho/src/Utils.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zoho;
+
+class Utils {
+
+    public static function normaliseArray($default = [], $payload = [])
+    {
+        $normalised = [];
+
+        foreach ($default as $key => $value) {
+            $normalised[$key] = isset($payload[$key]) ? $payload[$key] : $value;
+        }
+
+        return $normalised;
+    }
+
+    public static function toXmlEntity($entity, $payload = [])
+    {
+        $xml = "<$entity><row no='1'>";
+        foreach ($payload as $key => $value) {
+            $xml .= "<FL val='$key'>$value</FL>";
+        }
+        $xml .= "</row></$entity>";
+
+        return $xml;
+    }
+}
